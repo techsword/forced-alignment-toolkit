77d0e220fab53d74f965a9b6ae9ab0e8412728b4
Gaofei Shen <g.shen@tilburguniversity.edu>
2024-12-12 16:27:11 +0100

    minor bug fixes

--- generate_activations.py
+++ generate_activations.py
@@ -89,31 +89,43 @@ def slice_activations(
     slicing_tier = None if "slicing_tier" not in kwargs else kwargs["slicing_tier"]
 
     if slicing_tier is None:
-        return activation
+        return (
+            list(range(activation.hidden_state_activations.shape[-2])),
+            "no_slicing",
+            activation.hidden_state_activations,
+        )
     elif slicing_tier == "words" or slicing_tier == "phones":
         # Load textgrid file
         textgridfile = activation.filename.replace(".wav", ".TextGrid")
         tg = textgrids.TextGrid(textgridfile)
         wordtier = tg[slicing_tier]
-        sliced_activations = []
+        segment_label, sliced_activations = [], []
         for i, word in enumerate(wordtier):
             # print(word.text)
             # Turn xmins and xmaxs into wav2vec2 timesteps
             xmin_frame = int(word.xmin / 0.02)
             xmax_frame = int(word.xmax / 0.02)
-            sliced_activations.append(
-                SlicedActivations(
-                    slicename=word.text,
-                    hidden_state_activations=activation.hidden_state_activations[
-                        :, :, xmin_frame:xmax_frame
-                    ].mean(-2),
+            if xmin_frame == xmax_frame:
+                sliced_activations.append(
+                    activation.hidden_state_activations[:, :, xmin_frame]
                 )
-            )
-        return sliced_activations
+            else:
+                sliced_activations.append(
+                    activation.hidden_state_activations[
+                        :, :, xmin_frame:xmax_frame
+                    ].mean(-2)
+                )
+            segment_label.append(word.text)
+        return segment_label, slicing_tier, np.stack(sliced_activations, axis = -2)
     elif slicing_tier == "utterance":
-        return SlicedActivations(
-            slicename=activation.filename,
-            hidden_state_activations=activation.hidden_state_activations.mean(-2),
+        # return SlicedActivations(
+        #     slicename=activation.filename,
+        #     hidden_state_activations=activation.hidden_state_activations.mean(-2),
+        # )
+        return (
+            activation.filename,
+            slicing_tier,
+            activation.hidden_state_activations.mean(-2).unsqueeze(-2),
         )
 
     else:
@@ -143,9 +155,9 @@ def save_activations(**kwargs):
         "examples/activations" if "savepath" not in kwargs else kwargs["savepath"]
     )
 
-    output_file = (
-        f"{savepath}/{modelname.replace('/', '-')}-{os.path.dirname(datapath)}.pt"
-    )
+    slicing_tier = None if "slicing_tier" not in kwargs else kwargs["slicing_tier"]
+
+    output_file = f"{savepath}/{modelname.replace('/', '-')}-{os.path.dirname(datapath)}-{slicing_tier}.pt"
 
     if os.path.exists(output_file) and not kwargs.get("overwrite", False):
         print(f"Activations already exist at {output_file}. Skipping...")
@@ -175,8 +187,8 @@ if __name__ == "__main__":
     kwargs = {
         "modelname": "facebook/wav2vec2-base",
         "datapath": "examples/",
-        "slicing_tier": "words",
+        "slicing_tier": "phones",
         "savepath": "examples/activations",
-        "overwrite": False,
+        "overwrite": True,
     }
     save_activations(**kwargs)
